90b4069c09f2a29c8eaa6e3141478ed6d05ccba8
WillSmith4 <willsmith373737@gmail.com>
2024-10-02 20:49:42 +0200

    Update gapps.py

--- stuffs/gapps.py
+++ stuffs/gapps.py
@@ -5,12 +5,13 @@ from tools.helper import get_download_dir, host, print_color, run, bcolors
 
 class Gapps(General):
     dl_links = {
-            "x86_64": ["https://cfhcable.dl.sourceforge.net/project/opengapps/x86_64/20220503/open_gapps-x86_64-10.0-pico-20220503.zip", "5fb186bfb7bed8925290f79247bec4cf"],
-            "x86": ["https://udomain.dl.sourceforge.net/project/opengapps/x86/20220122/open_gapps-x86-10.0-pico-20220122.zip", "9e39e45584b7ade4529e6be654af7b81"],
-            "arm64-v8a": ["https://liquidtelecom.dl.sourceforge.net/project/opengapps/arm64/20220122/open_gapps-arm64-10.0-pico-20220122.zip", "8dfa6e76aeb2d1d5aed40b058e8a852c"],
-            "armeabi-v7a": ["https://nav.dl.sourceforge.net/project/opengapps/arm/20220122/open_gapps-arm-10.0-pico-20220122.zip", "a48ccbd25eb0a3c5e30f5db5435f5536"]
+            "x86_64": ["https://nchc.dl.sourceforge.net/project/opengapps/x86_64/20220503/open_gapps-x86_64-10.0-pico-20220503.zip", "5fb186bfb7bed8925290f79247bec4cf"],
+            "x86": ["https://cfhcable.dl.sourceforge.net/project/opengapps/x86/20220503/open_gapps-x86-10.0-pico-20220503.zip", "7fc75ec9bdca8def07bad306345ce877"],
+            "arm64-v8a": ["https://nchc.dl.sourceforge.net/project/opengapps/arm64/20220503/open_gapps-arm64-10.0-pico-20220503.zip", "2feaf25d03530892c6146687ffa08bc2"],
+            "armeabi-v7a": ["https://cfhcable.dl.sourceforge.net/project/opengapps/arm/20220215/open_gapps-arm-10.0-pico-20220215.zip", "1d00ffa4594734d477b10f2e0ee19c0b"]
         }
     arch = host()
+    print("arch: "+str(arch))
     download_loc = get_download_dir()
     dl_link = dl_links[arch[0]][0]
     dl_file_name = os.path.join(download_loc, "open_gapps.zip")
@@ -18,13 +19,22 @@ class Gapps(General):
     copy_dir = "./gapps"
     extract_to = "/tmp/ogapps/extract"
     non_apks = [
+        "vending-common.tar.lz",
         "defaultetc-common.tar.lz",
         "defaultframework-common.tar.lz",
         "googlepixelconfig-common.tar.lz"
         ]
+
+    if arch == ('x86_64', 64):
+        skip_1 = 'setupwizarddefault-x86_64.tar.lz'
+        skip_2 = "setupwizardtablet-x86_64.tar.lz"
+        
+    if arch == ('arm64-v8a', 64):
+        skip_1 = 'setupwizarddefault-arm64.tar.lz'
+        skip_2 = "setupwizardtablet-arm64.tar.lz"
     skip = [
-        "setupwizarddefault-x86_64.tar.lz",
-        "setupwizardtablet-x86_64.tar.lz"
+        skip_1,
+        skip_2
         ]
 
     def download(self):
